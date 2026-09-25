8e3671ff0128881741bff710b07625fbd97501d1
Brandon Barrett <bbarrett148@gmail.com>
2026-09-25 00:36:39 -0400

    Initial set of the loop for the program

--- Lab9_eomkid-1.py
+++ Lab9_eomkid-1.py
@@ -2,3 +2,12 @@
 Author: Brandon Barrett
 Description: This is a simple game about flipping coins between 2 players
 Date: September 27, 2026"""
+
+import player
+
+if __name__ == "__main__":
+    """This file runs the game. It creates the player objects and manages the game loop and rules."""
+    play_state = ""
+
+    while play_state != "n".lower():
+        pass
