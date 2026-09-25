ee185ecdb9c17ce87a27bb16970903f0ab5ff131
Brandon Barrett <bbarrett148@gmail.com>
2026-09-25 00:46:38 -0400

    The player objects needed to be moved to be outside of the game loop so they wouldnt be created each time it ran

--- Lab9_eomkid-1.py
+++ Lab9_eomkid-1.py
@@ -8,9 +8,9 @@ import player
 if __name__ == "__main__":
     """This file runs the game. It creates the player objects and manages the game loop and rules."""
     play_state = ""
+    player1 = player.Player()
+    player2 = player.Player(name="Player2")
 
     while play_state.upper() != "N":
         play_state = (
             input("Would you like to play a game of Coin Toss (Y/N)?\n"))
-        player1 = player.Player()
-        player2 = player.Player(name="Player2")
