28fdd79445227cdc46fc5423e00de61608c30861
Brandon Barrett <bbarrett148@gmail.com>
2026-09-24 22:45:54 -0400

    Creation of coin class and added import of randint for the purpose of pick a random value for the sideup attribute in the coin class

--- coin.py
+++ coin.py
@@ -1,3 +1,12 @@
 """Progam: Matching Coins Game - Coin Module
 Author: Brandon Barrett
 Description: This file contains the code for the coin class present in the main game"""
+
+from random import randint
+
+
+class Coin:
+    """This class represents a single, tossable coin. It is only aware of it's own state (Heads or Tails)."""
+
+    def __init__(self, __sideup):
+        self
