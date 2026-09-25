71081d5d25f25007bbb1467c91eb6c7d99c49b22
Brandon Barrett <bbarrett148@gmail.com>
2026-09-24 23:32:59 -0400

    The beginning of the Player class

--- player.py
+++ player.py
@@ -2,3 +2,13 @@
 Author: Brandon Barrett
 Description: This file contains the could necessary for the player in the main game file
 Date: September 27, 2026"""
+
+import coin
+"""The coin neccessary for the player to be able to interact with coin objects"""
+
+
+class Player:
+    """This class represents a player. A player has a name, has a wallet of coins, and has a Coin object to toss"""
+
+    def __init__(self):
+        __name = "Player1"
