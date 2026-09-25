7bfcc82812b756929be22353bd2129d478728bfa
Brandon Barrett <bbarrett148@gmail.com>
2026-09-24 23:17:59 -0400

    Coin class complete and test also all neccessary method have been created for the purpose of tossing the coin and returning that tossed value

--- coin.py
+++ coin.py
@@ -8,6 +8,16 @@ from random import randint
 class Coin:
     """This class represents a single, tossable coin. It is only aware of it's own state (Heads or Tails)."""
 
-    def __init__(self, __sideup):
+    def __init__(self):
         """Initialization of sideup attribute which will be used to determine the face of the coin given to the player."""
         self.__sideup = 0
+
+    def toss(self):
+        self.__sideup = randint(0, 1)
+
+    def get_sideup(self):
+        if self.__sideup == 0:
+            self.__sideup = "Heads"
+        else:
+            self.__sideup = "Tails"
+        return self.__sideup
